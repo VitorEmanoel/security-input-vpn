deb940f197adda66cbdf5eb4318085924c6b8e0a
Vitor Emanoel <vitor.amorim@libercapital.com.br>
2024-08-20 01:41:05 -0300

    fix: fix security group id

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "ubuntu_instance" {
   instance_type = var.ec2_instance_type
   key_name      = aws_key_pair.vpn_key_pair.key_name
   subnet_id     = aws_subnet.vpn_subnet.id
-  security_groups = [aws_security_group.vpn_security_group.name]
+  
+  vpc_security_group_ids = [ aws_security_group.vpn_security_group.id ]
 
   associate_public_ip_address = true
 
